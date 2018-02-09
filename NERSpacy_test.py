fd0f7e818d782f467c228d2f2d02db826dd28084
agilajah <febiagil20@gmail.com>
2018-02-09 10:41:54 +0700

    add new trained model

---
+++ NERSpacy_test.py
@@ -0,0 +1,10 @@
+        # test the saved model
+        print("Loading from", output_dir)
+        nlp2 = spacy.load(output_dir)
+        doc2 = nlp2(test_text)
+        for ent in doc2.ents:
+            print(ent.label_, ent.text)
+
+
+if __name__ == '__main__':
+    plac.call(main)
