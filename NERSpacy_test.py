07f1aabd93a20a635c31d498b0850a82f761142c
agilajah <febiagil20@gmail.com>
2018-02-09 10:49:04 +0700

    add test file

--- NERSpacy_test.py
+++ NERSpacy_test.py
@@ -1,10 +1,23 @@
-        # test the saved model
-        print("Loading from", output_dir)
-        nlp2 = spacy.load(output_dir)
-        doc2 = nlp2(test_text)
-        for ent in doc2.ents:
-            print(ent.label_, ent.text)
+#!/usr/bin/env python
+# coding: utf8
 
+from __future__ import unicode_literals, print_function
+
+import plac
+import random
+from pathlib import Path
+import spacy
+
+@plac.annotations(
+    output_dir=("Optional output directory", "option", "o", Path))
+def main(output_dir=None):
+        # test the saved model
+        test_text = 'Presiden terpilih  JOkO WidOdO   mengungkapkan pihaknya tidak akan membedakan spesifikasi kandidat menteri yang diusung Oleh partai pOlitik pengusung maupun pendukung.'
+        print("Loading from", output_dir)
+        nlp = spacy.load(output_dir)
+        doc = nlp(test_text)
+        for ent in doc.ents:
+            print(ent.label_, ent.text)
 
 if __name__ == '__main__':
     plac.call(main)
